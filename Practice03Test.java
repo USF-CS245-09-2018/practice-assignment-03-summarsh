b64a3dd4881b6c3279dbc10803b590ceb72dcb5d
Summer Marsh <summarsh1@gmail.com>
2018-09-14 14:44:06 -0700

    Add files via upload
    finished assignment

--- Practice03Test.java
+++ Practice03Test.java
@@ -53,12 +53,28 @@ public class Practice03Test {
 
 
 	public int find_min_iterative () {
-		// TODO: Fill in this iterative function.
+		int min = 0;
+		for(int i=1; i< arr.length; i++){
+			if(arr[i]<arr[min]){
+				min=i;
+			}
+		}
+		return min;
 	}
 
 
 	public int find_min_recursive () {
-		// TODO: Fill in this recursive function.
+		return find_min_recursive(0, 0);
+	}
+
+	public int find_min_recursive (int i, int min) {
+		if(i == arr.length-1) {
+			return min;
+		}
+		if(arr[i] < arr[min]) {
+			min=i;
+		}
+		return find_min_recursive(i+1, min);
 	}
 
 
